cb8f4cec975ae62a92f23c8049d0c6f447be3cda
Weijing Huang <weijinghuang@Weijings-Air.fios-router.home>
2022-09-05 12:32:03 -0400

    completed basic JS functions

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ const spendingTable = document.getElementById('spending-table');
 const spendingList = document.getElementById('spending-list'); 
 const doneBtn = document.getElementById('done-btn'); 
 const doneSpending = document.getElementById('done-spending');
-const totalSpending = document.getElementById('total-spending');
+const totalMessage = document.getElementById('total-message');
+const totalAmount = document.getElementById('total-amount');
+const finalMessage = document.getElementById('final-message');
+const amountList = [];
+
+let total = 0;
+
 
 // Load all event listeners
 loadEventListeners();
@@ -28,22 +34,26 @@ function loadEventListeners() {
 
     //Remove Item from list
     spendingList.addEventListener('click', removeItem);
+
+    // Submit Done
+    doneSpending.addEventListener('submit', submitDone);
+
 }
 
 
 function setGoal(e) {
-    let budget = parseFloat(inputBudget.value).toFixed(2);
+    let budget = parseInt(inputBudget.value);
    
     if(budget >= 50) {
        setMessage(`You've set your saving goal to $${budget}`, 'green');
-       inputBudget.value = '';
+       //inputBudget.value = '';
        submitBudget.value = 'Change Goal';
        spending.style.display = 'block';
        
     } else if(budget < 50) {
         setMessage(`Please try to save sometihng!!!`, 'red');
-        inputBudget.value = 'Please set your monthly goal';
-        spending.style.display = 'none';
+        //inputBudget.value = 'Please set your monthly goal';
+        //spending.style.display = 'none';
     }
 
     // Set Message
@@ -54,12 +64,9 @@ function setGoal(e) {
     e.preventDefault();
 };
 
-
-
-
 // Submit Spending
 function addSpending(e) {
-    let spendingAmout = parseFloat(amount.value).toFixed(2);
+    let spendingAmout = parseInt(amount.value);
 
     if(description.value === '' || spendingAmout === '' || spendingAmout < 0) {
         alert('Please add your spending.');
@@ -81,23 +88,13 @@ function addSpending(e) {
         // Append Child to list
         list.appendChild(row);
 
-        console.log(spendingAmout);
-        // Add Total Spending
-        let total = '';
-        // total += spendingAmout;
-        let spendingArr = []; 
-        spendingArr.push(spendingAmout);
-        console.log(spendingArr);
-        for (let i = 0; i < spendingArr.length; i++) {
-        total += spendingArr[i];
-        }
-        //Add total function
-       
-        
+        // Add total
+        amountList.push(spendingAmout);
+        if(amountList.length > 0) {
+        total = amountList.reduce((pre, curr) => pre + curr, 0); }
+        //addTotalAmount();
+        totalAmount.innerHTML = total;
 
-
-
-        // Set Total spending
         setMessage(`Total spending: $${total}`);
 
         // Display Spending Table
@@ -107,9 +104,8 @@ function addSpending(e) {
     // Set Message
     function setMessage(msg) {
         
-        totalSpending.innerHTML = msg;
+        totalMessage.innerHTML = msg;
     } 
-        
         description.value = '';
         amount.value = '';
     }
@@ -123,5 +119,27 @@ function removeItem(e) {
             e.target.parentElement.parentElement.remove();
         }
     }
+    e.preventDefault();
+}
+
+
+// Submit Done
+function submitDone(e) {
+    let totalExpense = parseInt(totalAmount.innerHTML);
+    let budget = parseInt(inputBudget.value);
+    let balance = Math.abs(budget - totalExpense);
+    
+    if(budget - totalExpense > 0) {
+        setMessage(`Congrats! You spent ${balance} less than your budget!`, 'green');
+    } else {
+      setMessage(`Ooops! You spent ${balance} more than your budget!`, 'red');
+    }
+    // Set Message
+    function setMessage(msg, color) {
+        finalMessage.innerHTML = msg;
+        finalMessage.style.color = color;
+    }
+    
+    
     e.preventDefault();
 }
